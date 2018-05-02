fba470e7b8af6e17dbd1ad65e7af06c9984f839e
rfc <reuben.fletchercostin@gmail.com>
2018-05-03 00:03:40 +1000

    distribute was deprecated. remove.

--- setup.py
+++ setup.py
@@ -4,15 +4,7 @@
 CmePy setup script
 """
 
-# ensure setuptools is present
-# n.b. no_fake flag should prevent this script
-#      from 'patching' the currently installed
-#      setuptools, if any
-
-from distribute_setup import use_setuptools
-## use_setuptools(no_fake = True)
-
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 
 from setuptools import setup, find_packages
 setup(
